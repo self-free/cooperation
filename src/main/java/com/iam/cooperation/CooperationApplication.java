934ce6383f701063b804bf69e3f8defd2ce6ad49
yl <hellsoul@yeah.net>
2018-09-12 16:23:53 +0800

    INIT

---
+++ src/main/java/com/iam/cooperation/CooperationApplication.java
@@ -0,0 +1,12 @@
+package com.iam.cooperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperationApplication.class, args);
+	}
+}
